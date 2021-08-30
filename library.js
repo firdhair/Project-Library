c8c754306497f5776f93b78c07fc3e351fd103c6
fira <firdhair30@gmail.com>
2021-08-30 23:14:22 +0700

    menambahkan fungsi delete dan edit

--- library.js
+++ library.js
@@ -3,9 +3,43 @@ const tableBooks = document.querySelector(".books");
 const bookTitle = document.querySelector(".book-title");
 const bookAuthor = document.querySelector(".book-author");
 const bookPages = document.querySelector(".book-pages");
+const booksInfo = Array.from(document.querySelectorAll(".books-info"));
+let myLibrary = [];
 
 addButton.addEventListener("click", addBookToLibrary);
-let myLibrary = [];
+tableBooks.addEventListener("click", bookCheck);
+
+function bookCheck(event) {
+  const item = event.target;
+  console.log(item.id);
+  // if trash button is pressed then delete the book's info
+  if (item.id === "trash-btn") {
+    const book = item.parentNode.parentNode;
+    book.remove();
+    console.log(book);
+  }
+  // if check button is pressed then change book's status to read
+  if (item.id === "complete-btn") {
+    const book = item.parentNode;
+    console.log(book);
+
+    const bookChild = book.children[0];
+    console.log(bookChild);
+
+    setTimeout(() => {
+      bookChild.setAttribute("class", "fas fa-check completed-btn");
+      bookChild.setAttribute("id", "completed-btn");
+    }, 50);
+  }
+
+  // if checked button is pressed then change book's status to havent completed read
+  if (item.id === "completed-btn") {
+    const book = item.parentNode;
+    const bookChild = book.children[0];
+    bookChild.setAttribute("class", "fas fa-times complete-btn");
+    bookChild.setAttribute("id", "complete-btn");
+  }
+}
 
 function Book() {
   // the constructor...
@@ -13,19 +47,16 @@ function Book() {
 
 function addBookToLibrary(event) {
   event.preventDefault();
-  console.log("yay");
   myLibrary.push(bookTitle.value, bookAuthor.value, bookPages.value);
   console.log(myLibrary);
-  // do stuff here
 
   // create new tr
   const newTr = document.createElement("tr");
   newTr.classList.add("books-info");
   tableBooks.appendChild(newTr);
-  console.log("yay2");
 
-  // create new td
-  // let newTd;
+  // create new td, complete button, and delete button
+  let newTd;
   for (let i = 0; i < myLibrary.length; i++) {
     newTd = document.createElement("td");
     newTd.innerText = myLibrary[i];
@@ -33,6 +64,21 @@ function addBookToLibrary(event) {
     newTr.appendChild(newTd);
   }
 
+  // Create mark button
+  newTd = document.createElement("td");
+  const completedButton = document.createElement("button");
+  completedButton.setAttribute("class", "fas fa-times complete-btn");
+  completedButton.setAttribute("id", "complete-btn");
+  newTd.appendChild(completedButton);
+  newTr.appendChild(newTd);
+
+  //Create Delete Button
+  newTd = document.createElement("td");
+  const deleteButton = document.createElement("i");
+  deleteButton.setAttribute("class", "fas fa-trash trash-btn");
+  deleteButton.setAttribute("id", "trash-btn");
+  newTd.appendChild(deleteButton);
+  newTr.appendChild(newTd);
+
   myLibrary = [];
-  // newTr.appendChild(newTd);
 }
