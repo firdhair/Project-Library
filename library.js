56fde04a829b52fb3961f9e668574dc4f10a32bc
fira <firdhair30@gmail.com>
2021-08-31 22:37:14 +0700

    added books' reading status

--- library.js
+++ library.js
@@ -13,24 +13,32 @@ const titleID = document.querySelector("#title");
 const authorID = document.querySelector("#author");
 const pagesID = document.querySelector("#pages");
 
+const readBooks = document.querySelector(".read-books");
+const unreadBooks = document.querySelector(".unread-books");
+const allBooks = document.querySelector(".all-books");
+
 let myLibrary = [];
 
-addButton.addEventListener("click", addBookToLibrary);
-tableBooks.addEventListener("click", bookCheck);
+let totalBook = 2;
+let hasRead = 0;
+let unRead = 2;
 
-function Book() {
-  // the constructor...
-}
+addButton.addEventListener("click", addBookToLibrary);
+tableBooks.addEventListener("click", bookSetting);
+
+showCase();
+
+// function Book() {
+// the constructor...
+// }
 
 function addBookToLibrary(event) {
   event.preventDefault();
   checkValidation();
-  // console.log(bookTitle.value === "");
   if (
     bookTitle.value === "" ||
     bookAuthor.value === "" ||
     bookPages.value === "" ||
-    bookPages.value.match(/[^1-9]/) ||
     bookPages.value <= 0
   ) {
     return;
@@ -60,43 +68,52 @@ function addBookToLibrary(event) {
       completedButton.setAttribute("id", "completed-btn");
       newTd.appendChild(completedButton);
       newTr.appendChild(newTd);
+      hasRead++;
+      totalBook++;
+      showCase();
     } else {
       newTd = document.createElement("td");
       const completedButton = document.createElement("button");
       completedButton.setAttribute("class", "fas fa-times complete-btn");
-      completedButton.setAttribute("id", "complete-btn");
+      //completedButton.setAttribute("id", "complete-btn");
       newTd.appendChild(completedButton);
       newTr.appendChild(newTd);
+      unRead++;
+      totalBook++;
+      console.log(totalBook);
+      showCase();
     }
 
     //Create Delete Button
     newTd = document.createElement("td");
-    const deleteButton = document.createElement("i");
+    const deleteButton = document.createElement("button");
     deleteButton.setAttribute("class", "fas fa-trash trash-btn");
-    deleteButton.setAttribute("id", "trash-btn");
+    //deleteButton.setAttribute("id", "trash-btn");
     newTd.appendChild(deleteButton);
     newTr.appendChild(newTd);
 
     myLibrary = [];
   }
+  showCase();
 }
 
 function checkValidation() {
+  // if input's value is empty then show required warning
   if (bookTitle.value === "") {
-    validationBook.setAttribute("id", "author-text");
+    validationBook.setAttribute("id", "title-text");
   }
   if (bookAuthor.value === "") {
-    validationAuthor.setAttribute("id", "pages-text");
+    validationAuthor.setAttribute("id", "author-text");
   }
-
   if (
     bookPages.value === "" ||
     bookPages.value.match(/[^1-9]/) ||
     bookPages.value <= 0
   ) {
-    validationPages.setAttribute("id", "title-text");
+    validationPages.setAttribute("id", "pages-text");
   }
 
+  // if input's value is not empty then do not show required warning
   if (bookTitle.value !== "") {
     validationBook.setAttribute("id", "author-text-checked");
   }
@@ -108,17 +125,28 @@ function checkValidation() {
   }
 }
 
-function bookCheck(event) {
+function bookSetting(event) {
   const item = event.target;
-  console.log(item.id);
+  console.log(item.classList[2]);
   // if trash button is pressed then delete the book's info
-  if (item.id === "trash-btn") {
+  if (item.classList[2] === "trash-btn") {
     const book = item.parentNode.parentNode;
     book.remove();
-    console.log(book);
+    const bookChild = book.children[3].childNodes[0];
+    console.log(bookChild);
+
+    // check if book hasnt been read then decrease unRead
+    if (bookChild.className === "fas fa-times complete-btn") {
+      unRead--;
+    } else if (bookChild.className === "fas fa-check completed-btn") {
+      hasRead--;
+    }
+
+    totalBook--;
+    showCase();
   }
   // if check button is pressed then change book's status to read
-  if (item.id === "complete-btn") {
+  if (item.classList[2] === "complete-btn") {
     const book = item.parentNode;
     console.log(book);
 
@@ -127,15 +155,27 @@ function bookCheck(event) {
 
     setTimeout(() => {
       bookChild.setAttribute("class", "fas fa-check completed-btn");
-      bookChild.setAttribute("id", "completed-btn");
     }, 50);
+    hasRead++;
+    unRead--;
+    showCase();
   }
 
   // if checked button is pressed then change book's status to havent completed read
-  if (item.id === "completed-btn") {
+  if (item.classList[2] === "completed-btn") {
     const book = item.parentNode;
     const bookChild = book.children[0];
     bookChild.setAttribute("class", "fas fa-times complete-btn");
     bookChild.setAttribute("id", "complete-btn");
+    hasRead--;
+    unRead++;
+    showCase();
   }
+}
+
+function showCase() {
+  readBooks.innerHTML = "Books read: " + hasRead;
+  unreadBooks.innerHTML = "Books unread: " + unRead;
+  allBooks.innerHTML = "Total Book: " + totalBook;
+  console.log(totalBook);
 }
